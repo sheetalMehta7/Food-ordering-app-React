a2a5fe782abed5beee253065acd975a41f11852a
Sheetal Mehta <107601193+sheetalMehta7@users.noreply.github.com>
2023-03-02 10:21:39 +0530

    Firebase Meals updated

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -11,7 +11,7 @@ const AvailableMeals = () => {
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        "https://react-food-odering-app-default-rtdb.firebaseio.com/meals.json"
+        "https://food-ordering-app-8b747-default-rtdb.firebaseio.com/meals.json"
       );
       if (!response.ok) {
         throw new Error("Something went wrong!");
